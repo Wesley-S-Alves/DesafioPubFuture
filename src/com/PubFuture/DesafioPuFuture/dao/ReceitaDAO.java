4b6f99f89f8ccc790f71d3fffa318a2ed1bc437a
Wesley-S-Alves <wesley.s.alves@outlook.com>
2022-01-14 06:09:16 -0300

    Contrução das Classes DAO Restantes

--- src/com/PubFuture/DesafioPuFuture/dao/ReceitaDAO.java
+++ src/com/PubFuture/DesafioPuFuture/dao/ReceitaDAO.java
@@ -15,8 +15,34 @@ public class ReceitaDAO {
         try {
             conn = new ConnectionMVC().getConnection();
             stmt = conn.prepareStatement(sql);
-
+            stmt.setDouble(1, receita.getValor());
+            stmt.setInt(2, receita.getTipoReceita().getId());
+            stmt.setInt(3,receita.getConta().getId());
+            stmt.setString(4, receita.getDescricao());
+            stmt.setDate(5, new Date(receita.getDataRecebimento().getTime()));
+            stmt.setDate(6, new Date(receita.getDataRecebimentoEsperado().getTime()));
+        }catch (SQLException e){
+                throw new ExceptionDAO(
+                        "Erro ao cadastrar a receita: " + e);
+            } finally {
+                try {
+                    if (stmt != null) {
+                        stmt.close();
+                    }
+                } catch (SQLException e) {
+                    throw new ExceptionDAO(
+                            "Erro ao fechar o Statement: " + e);
+                }
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException e) {
+                    throw new ExceptionDAO(
+                            "Erro ao fechar a conexão: " + e);
+                }
         }
     }
-
 }
+
+
